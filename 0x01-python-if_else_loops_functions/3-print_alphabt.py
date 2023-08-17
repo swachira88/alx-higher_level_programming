150816b4ede2ad78efb1d9fdf2361d38657826b5
swachira88 <113694419+swachira88@users.noreply.github.com>
2023-08-17 16:37:46 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if char != ord('q') and char != ord('e'):
-        print(chr(char), end='')
-
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
